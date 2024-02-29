e29853a452d9b82ee5ba90445bdc6bb2ba822524
Mohamed Ghazy <mohamedghazy@Mohameds-Air.fios-router.home>
2024-02-29 13:13:36 -0500

    second add

---
+++ HomeWork.java
@@ -0,0 +1,63 @@
+package com.neotech.lesson06;
+
+public class HomeWork {
+
+	public static void main(String[] args) {
+		
+	boolean seafood = true;	
+	boolean lactoes =false;	
+		
+	boolean peanut = false;	
+	
+	boolean hasAllergy = false;
+	
+	if(hasAllergy) {
+		if ( peanut)
+		{
+			System.out.println("do not eat peanut");
+		}	   
+	   if (lactoes) {
+		   System.out.println("do mot drink milk ");
+	   }
+	}
+	
+	else  {
+		System.out.println("you are healthy");
+	}
+	 
+		   
+	   
+	
+		
+		
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
